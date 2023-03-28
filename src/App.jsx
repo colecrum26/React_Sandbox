34465db09ba37408c09db810a34334802ad33dd0
colecrum26 <cwcrumrine@gmail.com>
2023-03-28 13:44:14 -0500

    practicing with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 // We import our HelloDiv from the components folder
 import HelloDiv from "./components/HelloDiv";
 
 // App is our top-level main component that references other components
 function App() {
+  const [greeting, setGreeting] = useState(["Heyo", "Yo what up!", "Peace"])
+
   return (
   <div>
     <HelloDiv />
+  <div>
+    <p>{greeting.map((element, index) => {
+      return <p>{element}</p>
+    })}</p>
+  </div>
   </div>
   );
 }
