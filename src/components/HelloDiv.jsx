f1e6d5992e58e8a0adc2dd6f82b6a5fb6c2a3afc
colecrum26 <cwcrumrine@gmail.com>
2023-03-28 11:34:30 -0500

    formatted

--- src/components/HelloDiv.jsx
+++ src/components/HelloDiv.jsx
@@ -15,4 +15,4 @@ return (
 }
 // TODO: Fix the export statement
 // Export statements are ES6 syntax that tell Javascript what "thing" we want to make available outside of this file
-export default HelloDiv
+export default HelloDiv;
