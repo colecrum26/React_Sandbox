a112d450b1bf918799efe491dc412b735204916a
colecrum26 <cwcrumrine@gmail.com>
2023-03-29 09:36:12 -0500

    formatting

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -6,18 +6,18 @@ import React from 'react';
 // This should be a string regarding what you think of React
 
 function JSXVariables() {
-  const name = "Cole";
-  const thoughts = "will be cool once I get the hang of the terms";
+  let name = "Cole";
+  let thoughts = "will be cool once I get the hang of the terms";
   return (
     <div className="main-container">
       <div className="container">
         <div className="jumbotron">
           {/* TODO: Add your name variable */}
-          <h1>Hi! My name is {name}</h1>
+          <h1>Hi! My name is {name}.</h1>
           {/* TODO: Add your number of letters variable */}
-          <h2>My name has {name.length} letters</h2>
+          <h2>My name has {name.length} letters.</h2>
           {/* TODO: Add your thoughts variable */}
-          <h2>I think React {thoughts}</h2>
+          <h2>I think React {thoughts}.</h2>
         </div>
       </div>
     </div>
