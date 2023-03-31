3da38a6fd42e356db391b97fec562cd63c53e888
colecrum26 <cwcrumrine@gmail.com>
2023-03-31 15:25:14 -0500

    tracking files and mods

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import Thermostat from './components/Thermostat';
+import React from "react";
+import Thermostat from "./components/Thermostat";
+import AboutMe from "./components/aboutMe";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
-  return <Thermostat />;
+  return (
+    <Router>
+      <Routes>
+        <Route 
+        path = "/"
+        element = {<Thermostat />}
+        />
+        <Route
+        path = "/About"
+        element = {<AboutMe />}
+        />
+      </Routes>
+      <Link to="/About">About</Link>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/aboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe(){
+    return (
+        <div>
+        <h1>Hey I'm Cole</h1>
+        <h2>I'm learning how to use React</h2>
+        </div>
+    )
+}
+
+export default AboutMe;
